ad5f124a838a8217d188a1f1ff405afe62861ba6
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2022-01-10 17:08:10 -0300

    Used API to handling requisitions methods

---
+++ src/pages/api/clients/index.js
@@ -0,0 +1,18 @@
+export default function handler(req, res){
+
+  if(req.method === 'GET'){
+    handleGet(req, res);
+  } else {
+    res.status(405).send();
+  }
+
+}
+
+function handleGet(req, res){
+  res.status(204).json({
+    id: 001,
+    nameUser: 'Isaac',
+    email: 'isaacadm@adm.com', 
+    admin: true
+  });
+}
